d9c6c257a8299bd71fddc5f902fc89cf6df619f8
Naxim Metel <maxmetel@gamil.com>
2017-10-17 11:03:20 +0600

    Mappers, controller stub

--- src/DAL/UserDAL.java
+++ src/DAL/UserDAL.java
@@ -1,5 +1,20 @@
 package DAL;
 
-public class UserDAL {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import models.User;
+
+public class UserDAL extends BaseDAL {
+	public List<User> selectAll() {
+		SqlSession session = getSqlSessionFactory().openSession();
+		return session.selectList("user.selectAll");
+	}
+	
+	public User selectById(int id) {
+		SqlSession session = getSqlSessionFactory().openSession();
+		return session.selectOne("user:selectById");
+	}
 }
