b9ca81e2d89f9271df3b5668593e1e6df31ce1d6
Nick Filatov <keddofilatov@gmail.com>
2018-01-10 00:17:23 +0600

    [+] registrations form

---
+++ client/src/routes/Registration.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Container, Header, Form, Button, Dropdown, Input, TextArea } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Author from '../components/Author';
+import API from '../Api';
+// private int id;
+// private String isbn;
+// private String title;
+// private List<Author> authors = new ArrayList<>();
+// private List<Category> cateries = new ArrayList<>()
+// private Publisher publisher;
+// private int year;
+// private int count;
+// private int size;
+// private String description;
+// private Date addedAt;
+class CreateBook extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  changeInputValue = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  createNewUser = e => {
+    console.log(this.state);
+  };
+
+  render() {
+    const { email, password } = this.state;
+    const { changeInputValue, createNewUser } = this;
+    return (
+      <Wrapper text>
+        <Header>Регистрация</Header>
+        <Form>
+          <Form.Field>
+            <label>Email</label>
+            <input name="email" onChange={changeInputValue} value={email} placeholder="Email" />
+          </Form.Field>
+          <Form.Field>
+            <label>Пароль</label>
+            <input
+              name="password"
+              type="password"
+              onChange={changeInputValue}
+              value={password}
+              placeholder="Пароль"
+            />
+          </Form.Field>
+          <Button type="submit" onClick={createNewUser}>
+            Submit
+          </Button>
+        </Form>
+      </Wrapper>
+    );
+  }
+}
+
+const Wrapper = styled(Container)`
+  margin-top: 50px;
+  margin-bottom: 50px;
+`;
+
+export default CreateBook;
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Book from './Book';
 import CreateBook from './CreateBook';
+import Registration from './Registration';
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/registration" exact component={Registration} />
       <Route path="/book/view/:bookId" component={Book} />
       <Route path="/book/create" component={CreateBook} />
     </Switch>
