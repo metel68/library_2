1611a3eeb58829e63bc15fc852f6d3b0b503c89f
Nick Filatov <keddofilatov@gmail.com>
2018-01-11 21:14:26 +0600

    [-] service worker

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
-registerServiceWorker();
